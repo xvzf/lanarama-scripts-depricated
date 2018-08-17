4326fe3fffff9367c70c07f09d396a443d21e4fd
Matthias Riegler <riegler.matthias2@gmail.com>
2018-08-17 19:44:55 +0200

    Added example scripts

---
+++ deploy_csgo_server.sh
@@ -0,0 +1,2 @@
+python deploy-csgo.py --servername=dockerhost0 --ip-start=192.168.10.20 --server-count=10 --rcon-password=dontusethispassword
+# python deploy-csgo.py --servername=dockerhost1 --ip-start=192.168.10.40 --server-count=10 --rcon-password=dontusethispassword
---
+++ dockerhost0_network_example.sh
@@ -0,0 +1,2 @@
+# RRbond docker network
+# docker network create -d macvlan --subnet="192.168.10.0/24" --gateway="192.168.10.1" --ip-range="192.168.10.20/" -o parent="rrbond" servernetwork
